21f889b8a00f0151efe965eb8b37cbbe1a9585da
Lumotheninja <wentat126@yahoo.com>
2017-11-19 23:37:46 +0800

    Added new features

--- Itinerick3/app/src/main/java/com/example/owner/itinerick3/BigHead.java
+++ Itinerick3/app/src/main/java/com/example/owner/itinerick3/BigHead.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class BigHead extends AppCompatActivity {
 
@@ -19,36 +20,27 @@ public class BigHead extends AppCompatActivity {
         ImageView imageMerlion = (ImageView) findViewById(R.id.merlion);
         imageGardens.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent intent = new Intent(BigHead.this,Itinerick3.class);
+                Intent intent = new Intent(BigHead.this, Itinerick3.class);
                 startActivity(intent);
             }
-
         });
         imageUss.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent intent = new Intent(BigHead.this,Itinerick3.class);
+                Intent intent = new Intent(BigHead.this, Itinerick3.class);
                 startActivity(intent);
             }
-
         });
         imageSentosa.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent intent = new Intent(BigHead.this,Itinerick3.class);
+                Intent intent = new Intent(BigHead.this, Itinerick3.class);
                 startActivity(intent);
             }
-
         });
         imageMerlion.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent intent = new Intent(BigHead.this,Itinerick3.class);
+                Intent intent = new Intent(BigHead.this, Itinerick3.class);
                 startActivity(intent);
             }
-
         });
     }
-
-
-
-
-
 }
