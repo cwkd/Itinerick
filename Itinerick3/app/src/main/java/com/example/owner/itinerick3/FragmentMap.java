466b8a8d322514427a91e066b209f1916a413883
cwkd <daniel_chin@mymail.sutd.edu.sg>
2017-12-04 20:13:16 +0800

    added map waypointing and route highlighting. included distance and travel times.

--- Itinerick3/app/src/main/java/com/example/owner/itinerick3/FragmentMap.java
+++ Itinerick3/app/src/main/java/com/example/owner/itinerick3/FragmentMap.java
@@ -3,6 +3,7 @@ package com.example.owner.itinerick3;
 import android.content.Intent;
 import android.location.Address;
 import android.location.Geocoder;
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -14,11 +15,22 @@ import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.maps.DirectionsApi;
+import com.google.maps.GeoApiContext;
+import com.google.maps.android.PolyUtil;
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.DirectionsResult;
+import com.google.maps.model.TravelMode;
+
+import org.joda.time.DateTime;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 public class FragmentMap extends FragmentActivity implements OnMapReadyCallback {
 
@@ -35,8 +47,8 @@ public class FragmentMap extends FragmentActivity implements OnMapReadyCallback 
         mapFragment.getMapAsync(this);
         Intent intent = getIntent();
         dataset = intent.getStringArrayListExtra(ActivityLocation.LIST_OF_PLACES);
-    }
 
+    }
 
     /**
      * Manipulates the map once available.
@@ -59,11 +71,14 @@ public class FragmentMap extends FragmentActivity implements OnMapReadyCallback 
         settings.setMapToolbarEnabled(true);
         LatLng sg = new LatLng(1.352083, 103.819836);
         if (dataset.isEmpty()) {
-            // Add a marker in Sydney and move the camera
+            // Add a marker in SG and move the camera
 
             mMap.addMarker(new MarkerOptions().position(sg).title("Marker in Singapore"));
 
-        } else {
+        } else if (dataset.contains("Rick")) {
+            mMap.addMarker(new MarkerOptions().position(sg).title("Marker in Singapore"));
+
+        } /*else {
             Geocoder geocoder = new Geocoder(this, Locale.getDefault());
             List<Address> addresses;
             ArrayList<Marker> markers = new ArrayList<>();
@@ -74,13 +89,70 @@ public class FragmentMap extends FragmentActivity implements OnMapReadyCallback 
                     LatLng coordinates = new LatLng(address.getLatitude(), address.getLongitude());
                     Marker marker = mMap.addMarker(new MarkerOptions().position(coordinates).title("Marker of " + location));
                     markers.add(marker);
-                } catch (IOException e) {
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
-            mMap.moveCamera(CameraUpdateFactory.newLatLng(sg));
-            mMap.moveCamera(CameraUpdateFactory.zoomTo(12.0f));
 
+
+        }*/
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sg));
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(12.0f));
+
+        Iterator<String> iterator;
+        String lastLocation;
+        String currentLocation;
+        iterator = dataset.iterator();
+        lastLocation = iterator.next();
+        while (iterator.hasNext()) {
+            currentLocation = iterator.next();
+            DateTime now = new DateTime();
+            try {
+                DirectionsResult result = DirectionsApi.newRequest(getGeoContext())
+                        .mode(TravelMode.DRIVING).origin(lastLocation)
+                        .destination(currentLocation).departureTime(now).await();
+                addMarkersToMap(result, mMap);
+                addPolyline(result, mMap);
+            } catch (ApiException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            lastLocation = currentLocation;
         }
+    }
+
+    public GeoApiContext getGeoContext() {
+        GeoApiContext geoApiContext = new GeoApiContext();
+        return geoApiContext.setQueryRateLimit(3)
+                .setApiKey(getString(R.string.directionsApiKey))
+                .setConnectTimeout(1, TimeUnit.SECONDS)
+                .setReadTimeout(1, TimeUnit.SECONDS)
+                .setWriteTimeout(1, TimeUnit.SECONDS);
+    }
+
+    public void addMarkersToMap(DirectionsResult results, GoogleMap mMap) {
+        mMap.addMarker(new MarkerOptions().position(new LatLng(results
+                .routes[0].legs[0].startLocation.lat,
+                results.routes[0].legs[0].startLocation.lng))
+                .title(results.routes[0].legs[0].startAddress));
+        mMap.addMarker(new MarkerOptions().position(new LatLng(results
+                .routes[0].legs[0].endLocation.lat,
+                results.routes[0].legs[0].endLocation.lng))
+                .title(results.routes[0].legs[0].startAddress)
+                .snippet(getEndLocationTitle(results)));
+    }
+
+    public String getEndLocationTitle(DirectionsResult results){
+        return  "Time :"+ results.routes[0].legs[0].duration.humanReadable
+                + " Distance :" + results.routes[0].legs[0].distance.humanReadable;
+    }
+
+    public void addPolyline(DirectionsResult results, GoogleMap mMap) {
+        List<LatLng> decodedPath = PolyUtil.decode(results.routes[0]
+                .overviewPolyline.getEncodedPath());
+        mMap.addPolyline(new PolylineOptions().addAll(decodedPath));
     }
 }
